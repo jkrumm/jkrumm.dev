e8fa3ae4c0b2cab52d464036b5554fcb95660283
jkrumm <jkrumm@pm.me>
2025-01-20 18:43:33 +0100

    feat(prettier): install prettier

---
+++ .prettierrc.mjs
@@ -0,0 +1,21 @@
+// .prettierrc.mjs
+/** @type {import('prettier').Config} */
+export default {
+	useTabs: true,
+	singleQuote: true,
+	trailingComma: 'none',
+	semi: false,
+	printWidth: 100,
+	plugins: ['prettier-plugin-astro', 'prettier-plugin-tailwindcss'],
+	tailwindStylesheet: './styles/root.css',
+	tailwindFunctions: ['clsx', 'tw'],
+	tailwindConfig: './tailwind.config.cjs',
+	overrides: [
+		{
+			files: '*.astro',
+			options: {
+				parser: 'astro'
+			}
+		}
+	]
+}
--- src/constants.ts
+++ src/constants.ts
@@ -1,14 +1,27 @@
 export const SITE = {
-    NAME: "jkrumm.dev",
-    EMAIL: "jkrumm@pm.me",
-} as const;
+	NAME: 'jkrumm.dev',
+	EMAIL: 'jkrumm@pm.me',
+	BIRTHDAY: new Date('1997-05-29'),
+	get AGE() {
+		const today = new Date()
+		let age = today.getFullYear() - this.BIRTHDAY.getFullYear()
+		const monthDifference = today.getMonth() - this.BIRTHDAY.getMonth()
+		if (
+			monthDifference < 0 ||
+			(monthDifference === 0 && today.getDate() < this.BIRTHDAY.getDate())
+		) {
+			age--
+		}
+		return age
+	}
+} as const
 
 type Metadata = {
-    TITLE: string;
-    DESCRIPTION: string;
+	TITLE: string
+	DESCRIPTION: string
 }
 
 export const HOME: Metadata = {
-    TITLE: "Home",
-    DESCRIPTION: "Welcome to my personal website!"
-};
+	TITLE: 'Home',
+	DESCRIPTION: 'Welcome to my personal website!'
+}
