dadbcdb96891f3c31753c52a708745e033d4d09c
jkrumm <jkrumm@pm.me>
2025-01-30 18:10:35 +0100

    feat(style): improved font colors and theme change

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -12,18 +12,32 @@ module.exports = {
 			},
 			colors: {
 				dark: {
-				0: '#C9C9C9',
-				1: '#b8b8b8',
-				2: '#828282',
-				3: '#696969',
-				4: '#424242',
-				5: '#3b3b3b',
-				6: '#2e2e2e',
-				7: '#242424',
-				8: '#1f1f1f',
-				9: '#141414',
+					0: '#C9C9C9',
+					1: '#b8b8b8',
+					2: '#828282',
+					3: '#696969',
+					4: '#424242',
+					5: '#3b3b3b',
+					6: '#2e2e2e',
+					7: '#242424',
+					8: '#1f1f1f',
+					9: '#141414'
 				}
-			}
+			},
+			typography: (theme) => ({
+				DEFAULT: {
+					css: {
+						color: theme('colors.dark.7')
+					}
+				},
+				dark: {
+					css: [
+						{
+							color: theme('colors.stone.300')
+						}
+					]
+				}
+			})
 		}
 	},
 	plugins: [require('@tailwindcss/typography')]
